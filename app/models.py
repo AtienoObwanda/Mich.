1f1f711bafd75bb583df8345f653180088872b69
atienoobwanda <atienoobwanda@gmail.com>
2022-06-11 07:23:10 +0300

    Updating projects model, adding cloudinary to my apps

--- app/models.py
+++ app/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Project(models.Model):
     projectName = models.CharField(max_length=30)
     projectImage = models.ImageField
     projectLink = models.URLField
     projectDescription = models.TextField
-    uploadDate = models.DateTimeField(auto_now_add=True)
+    projectCategory = models.CharField(max_length=60)
+    projectTechnology = models.CharField(max_length=60)
+    projectOwner= models.ForeignKey(User, on_delete=models.CASCADE)
+    uploadDate = models.DateTimeField(default=timezone.now)
 
+
+
+# Find out whether I need to add tags
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary',
+    'users',
+    'app',
 ]
 
 MIDDLEWARE = [
