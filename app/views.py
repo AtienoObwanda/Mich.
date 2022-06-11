e3e7765bf526ae5d15cff22293af87962ab92a4b
atienoobwanda <atienoobwanda@gmail.com>
2022-06-11 12:00:42 +0300

    Adding a view for the serializer

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from  rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import *
+from .serializer import ProjectSerializer
 
-# Create your views here.
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        allProjects = Project.objects.all()
+        serializers = ProjectSerializer(allProjects, many=True)
+        return Response(serializers.data)
