90f316d53134a0a4c779c7acd0de82dd4ca53a08
atienoobwanda <atienoobwanda@gmail.com>
2022-06-11 07:11:41 +0300

    adding a model for the projects

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    projectName = models.CharField(max_length=30)
+    projectImage = models.ImageField
+    projectLink = models.URLField
+    projectDescription = models.TextField
+    uploadDate = models.DateTimeField(auto_now_add=True)
+
