12acfc872342e53a7ac6e9351b1f119e0129880b
atienoobwanda <atienoobwanda@gmail.com>
2022-06-11 15:17:13 +0300

    able to post projects

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/projects/', views.ProjectList.as_view()),
+    path('api/projects/', views.TestProjectApi.as_view()),
     path('', views.AllProjectList.as_view(), name='projects' ),
     path('project/<int:pk>/', views.ProjectDetailList.as_view(),name='projectDetail'),
+    path('post/project/', views.AddProject.as_view(),name='addProject'),
+
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,10 +3,32 @@ from  rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import status
+from django.http import HttpResponseRedirect
+
 
 
 from .models import *
 from .serializer import ProjectSerializer
+
+
+  # TESTING PROJECT ON POSTMAN
+class TestProjectApi(APIView):
+
+    def get(self, request, format=None):
+        projects = Project.objects.all()
+        serializers = ProjectSerializer(projects, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data,status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
 
 class AllProjectList(APIView):
     renderer_classes = [TemplateHTMLRenderer]
@@ -24,20 +46,21 @@ class ProjectDetailList(APIView):
         project = get_object_or_404(Project, pk=pk)
         return Response({'project': project})
 
+class AddProject(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'add_project.html'
+    model = Project
+    def get(self, request):
+        serializer = ProjectSerializer()
+        return Response({'serializer': serializer})
 
+    def post(self, request):
+        # project = get_object_or_404(Project, pk=pk)
+        serializer = ProjectSerializer( data = request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer})
+        serializer.save()
 
+        return HttpResponseRedirect('') # Configure to return the auther profile
 
-    
-class ProjectList(APIView):
-
-    def get(self, request, format=None):
-        projects = Project.objects.all()
-        serializers = ProjectSerializer(projects, many=True)
-        return Response(serializers.data)
-
-    def post(self, request, format=None):
-        serializers = ProjectSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data,status=status.HTTP_201_CREATED)
-        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+  
