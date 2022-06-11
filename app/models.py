41f96ae608c164600588db8719e01db16f5d2ba2
atienoobwanda <atienoobwanda@gmail.com>
2022-06-11 11:45:46 +0300

    Adding class methods for the project model and adding the tests for the projects model | All tests passed

--- app/models.py
+++ app/models.py
@@ -9,8 +9,8 @@ from cloudinary.models import CloudinaryField
 class Project(models.Model):
     projectName = models.CharField(max_length=30)
     projectImage = CloudinaryField('image')
-    projectLink = models.URLField
-    projectDescription = models.TextField
+    projectLink = models.URLField(max_length=200, null=True, blank=True)
+    projectDescription = models.TextField(max_length=500, blank=True, default=f'Project Description')
     projectCategory = models.CharField(max_length=60)
     projectTechnology = models.CharField(max_length=60)
     projectOwner= models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,6 +18,38 @@ class Project(models.Model):
 
     def __str__(self):
             return self.projectName
+
+    def saveProject(self):
+        self.save()
+
+    def deleteProject(self):
+        self.delete()
+    
+    def updateProjectName(self, new_projectName):
+        self.projectName = new_projectName
+        self.save()
+
+    @classmethod
+    def getProjectId(cls, project_id):
+        project = Project.objects.filter(project_id=project_id).all()
+        return project
+
+    @classmethod
+    def getUserProjects(cls, user_id):
+        userProject = Project.objects.filter(user_id=user_id).all()
+        return userProject
+
+    @classmethod
+    def getProjects(cls):
+        allProjects = cls.objects.all()
+        return allProjects
+
+    @classmethod
+    def searchProject(cls, search_term):
+        projectResults = Project.objects.filter(sitename__icontains=search_term)
+
+    
+
 
 
 # Find out whether I need to add tags
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import *
+
+
+# Tests for the user profile are in the users application
+
+
+
+class ProjectTest(TestCase):
+    def setUp(self):
+        self.test_user = User(username='Atieno', password='test123')
+        self.test_user.save()
+
+        self.test_project = Project(projectName ='Test Project Title', projectImage='demo.png',projectLink='www.example.com',projectDescription='Test project description',projectCategory = '', projectTechnology = '',projectOwner=self.test_user,uploadDate = datetime.now())
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.test_project, Project))
+
+    def test_saveProject(self):
+        self.test_project.saveProject()
+        self.assertEqual(len(Project.objects.all()),1)
+
+  
+
+    def test_updateProjectName(self):
+        self.test_project.updateProjectName( new_projectName='Updated Project Name')
+        self.test_project.saveProject()
+
+    def test_getProjectId(self):
+        test_id = 1
+        getProject = Project.objects.filter(id=test_id)
+        self.assertEqual(len(getProject),0)
+
+    def tearDown(self):
+        self.test_user.delete()
+        Project.objects.all().delete()
+
