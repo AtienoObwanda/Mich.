7f61f2ba593f870acb652603c39e12dd51fe76b6
atienoobwanda <atienoobwanda@gmail.com>
2022-06-12 19:40:02 +0300

    updating homepage styling

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 
 
     path('register', views.RegisterUser.as_view(), name='register'),
-    #     path('register', views.RegisterUser.as_view(), name='register'),
+    path('login/', views.LoginUser.as_view(), name = 'login'),
+    path('user/', views.ProfileView.as_view(), name='account'),
+    path('logout/', views.LogoutView.as_view()),
 
 ]
