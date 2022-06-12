eb869983e5178c95ffc4b32545eb08c3771c10dc
atienoobwanda <atienoobwanda@gmail.com>
2022-06-12 16:48:48 +0300

    adding user view and logout view

--- users/views.py
+++ users/views.py
@@ -49,6 +49,55 @@ class LoginUser(APIView):
         return response
 
 
+class ProfileView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Invalid Session!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithm=['HS256'])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Session expired!')
+            
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie(jwt)
+        response.data = {
+            'message' : 'success'
+        }
+        return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
