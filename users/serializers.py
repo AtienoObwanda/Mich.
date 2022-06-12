7cd401408aeae796d8afde7d85f2783bcef82600
atienoobwanda <atienoobwanda@gmail.com>
2022-06-12 16:39:26 +0300

    creating api view for login

--- users/serializers.py
+++ users/serializers.py
@@ -4,19 +4,20 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields =['id', 'username', 'email']
-        
+        USERNAME_FIELD = 'email'
+        username = None
 
-class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields =['id', 'username', 'email']
+        fields =['id', 'email', 'password']
         extra_kwargs ={
             'password': {'write_only' : True},
-            'email': {'unique' : True}
         }
 
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
 
-        return user
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.RegisterUser.as_view(), name='register'),
+    path('api/register/', views.RegisterUser.as_view()),
+    path('api/login/', views.LoginUser.as_view()),
+
     path('register', views.RegisterUser.as_view(), name='register'),
     #     path('register', views.RegisterUser.as_view(), name='register'),
 
