46b8e7a763aef7ac58cfa6a9cb316b50c9ce1c14
atienoobwanda <atienoobwanda@gmail.com>
2022-06-13 05:00:04 +0300

    moving project view from app to users app. THis is to streamline the authentication process

--- app/views.py
+++ app/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, get_object_or_404
 from  rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import status
-from django.http import HttpResponseRedirect
 
 
 
@@ -38,59 +36,3 @@ class AllProjectList(APIView):
         projects = Project.objects.all()
         return Response({'projects':projects})
 
-class ProjectDetailList(APIView):
-    renderer_classes= [TemplateHTMLRenderer]
-    template_name='projects/project_detail.html'
-
-    def get(self, request, pk):
-        project = get_object_or_404(Project, pk=pk)
-        return Response({'project': project})
-
-class AddProject(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'add_project.html'
-    model = Project
-    def get(self, request):
-        serializer = ProjectSerializer()
-        return Response({'serializer': serializer})
-
-    def post(self, request):
-        # project = get_object_or_404(Project, pk=pk)
-        serializer = ProjectSerializer( data = request.data)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer})
-        serializer.save()
-
-        return HttpResponseRedirect('') # Configure to return the auther profile
-
-class UpdatePoject(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'projects/update_project.html'
-
-    def get(self, request, pk):
-        project = get_object_or_404(Project, pk=pk)
-        serializer = ProjectSerializer(project)
-        return Response({'serializer': serializer, 'project': project})
-
-    def put(self, request, pk):
-        profile = get_object_or_404(Project, pk=pk)
-        serializer = ProjectSerializer(profile, data=request.data)
-        if not serializer.is_valid():
-            return Response({'serializer': serializer, 'profile': profile})
-        serializer.save()
-        return HttpResponseRedirect('project')
-
-
-class DeleteProject(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'projects/delete_project.html'
-
-    def get(self, request, pk):
-        project = get_object_or_404(Project, pk=pk)
-        serializer = ProjectSerializer(project)
-        return Response({'serializer': serializer, 'project': project})
-    
-    def delete(self, request, pk):
-        profile = get_object_or_404(Project, pk=pk)
-        profile.delete()
-        return HttpResponseRedirect('') #  Configure to return the auther profile
--- users/views.py
+++ users/views.py
@@ -1,13 +1,19 @@
 from re import A
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response    
 from rest_framework.exceptions import AuthenticationFailed
 import jwt, datetime
 from rest_framework.renderers import TemplateHTMLRenderer
+from django.http import HttpResponseRedirect
 
 
 from .serializers import *
+from app.models import *
+from app.serializer import ProjectSerializer
+
+
+
 
 
 class RegisterUser(APIView):
@@ -98,6 +104,64 @@ class LogoutView(APIView):
 
 
 
+# project related views
+
+class ProjectDetailList(APIView):
+    renderer_classes= [TemplateHTMLRenderer]
+    template_name='projects/project_detail.html'
+
+    def get(self, request, pk):
+        project = get_object_or_404(Project, pk=pk)
+        return Response({'project': project})
+
+class AddProject(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'add_project.html'
+    model = Project
+    def get(self, request):
+        serializer = ProjectSerializer()
+        return Response({'serializer': serializer})
+
+    def post(self, request):
+        # project = get_object_or_404(Project, pk=pk)
+        serializer = ProjectSerializer( data = request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer})
+        serializer.save()
+
+        return HttpResponseRedirect('') # Configure to return the auther profile
+
+class UpdatePoject(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'projects/update_project.html'
+
+    def get(self, request, pk):
+        project = get_object_or_404(Project, pk=pk)
+        serializer = ProjectSerializer(project)
+        return Response({'serializer': serializer, 'project': project})
+
+    def put(self, request, pk):
+        profile = get_object_or_404(Project, pk=pk)
+        serializer = ProjectSerializer(profile, data=request.data)
+        if not serializer.is_valid():
+            return Response({'serializer': serializer, 'profile': profile})
+        serializer.save()
+        return HttpResponseRedirect('project')
+
+
+class DeleteProject(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'projects/delete_project.html'
+
+    def get(self, request, pk):
+        project = get_object_or_404(Project, pk=pk)
+        serializer = ProjectSerializer(project)
+        return Response({'serializer': serializer, 'project': project})
+    
+    def delete(self, request, pk):
+        profile = get_object_or_404(Project, pk=pk)
+        profile.delete()
+        return HttpResponseRedirect('') #  Configure to return the auther profile
 
 
 
