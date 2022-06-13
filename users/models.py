931734e019d61e6faacfd1826db24cc384704ed0
atienoobwanda <atienoobwanda@gmail.com>
2022-06-13 14:17:21 +0300

    Adding a proofile Serializer

--- users/models.py
+++ users/models.py
@@ -16,14 +16,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username}profile'
 
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.profileImage.path)
-        if img.height > 300 or img.width > 300:
-            output_size=(300,300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     img = Image.open(self.profileImage.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size=(300,300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
 
     def save_profile(self):
         self.save()
@@ -47,7 +47,7 @@ class Profile(models.Model):
     
 
     '''
-    
+
     Django signals for the Profile model are stored in the signal.py
     
     '''
--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+
+from . models import Profile
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -29,3 +32,18 @@ class LoginSerializer(serializers.Serializer):
         max_length=100,
         style={'input_type': 'password', 'placeholder': 'Password'}
     )
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = "__all__"
+        
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+
+
+    
+
