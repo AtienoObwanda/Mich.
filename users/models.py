2953a3af30757788c8bb70349d9979814efaaec4
atienoobwanda <atienoobwanda@gmail.com>
2022-06-15 06:24:58 +0300

    configuring settings file for deployment

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from django.dispatch import receiver
 from PIL import Image
 from django.urls import reverse
 
@@ -11,6 +13,17 @@ class Profile(models.Model):
     country = models.CharField(max_length=50)
     profileImage = models.ImageField(default='default.png',upload_to='projectPics')
     bio=models.TextField(max_length=500, blank=True, default=f'Inspired to Code...')
+
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_profile(sender, instance, **kwargs):
+        
+        instance.profile.save()
 
 
     def __str__(self):
